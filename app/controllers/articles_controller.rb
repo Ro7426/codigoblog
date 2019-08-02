b6d89430ea19b2dc1d3f712eeb32fae8cb314309
Rodrigo Gonzalez Hernandez <jordan23rodrigo@gmail.com>
2019-08-02 13:41:27 -0400

    model and controller article

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ApplicationController
+
+
+def new
+    @article = Article.new
+end
+
+def create
+    @article = Article.create(title: params[:article][:title])
+    render json: @article
+end
+
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get "/bienvenida", to: "home#index"
+
+get "articles/new", to: "articles#new"
+post "articles", to: "articles#create"
+end
