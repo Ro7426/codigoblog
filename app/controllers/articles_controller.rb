1f700683e141a01b5c4cdc43becc9274da3e0041
Rodrigo Gonzalez Hernandez <jordan23rodrigo@gmail.com>
2019-08-03 19:21:42 -0400

    before action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
     # before_action :find_article, only: [:show,:edit,:update,:destroy]
     before_action :find_article, except: [:new,:create]
-
+    
 
 
 def show
