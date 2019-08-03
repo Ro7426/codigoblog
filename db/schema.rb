3b72b327455e61fb29c098760f134a6996165d25
Rodrigo Gonzalez Hernandez <jordan23rodrigo@gmail.com>
2019-08-03 18:07:51 -0400

    controller articles

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2019_08_02_172059) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "content"
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
